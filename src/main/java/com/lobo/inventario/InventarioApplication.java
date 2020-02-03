f2c4ba3720c0b48df2361604486c94d66007a1a1
Fernando Lobos <fernandolobos@gmail.com>
2020-02-03 11:03:18 -0300

    git ignore logs

--- src/main/java/com/lobo/inventario/InventarioApplication.java
+++ src/main/java/com/lobo/inventario/InventarioApplication.java
@@ -10,12 +10,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan("com.lobo.inventario")
 public class InventarioApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
 	}
-
-
-
 }
